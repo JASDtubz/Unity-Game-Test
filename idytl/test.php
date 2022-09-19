edc089f441e1636890d69e22fdfe04962937e75d
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-09-19 13:06:37 -0500

    Create test.php

---
+++ idytl/test.php
@@ -0,0 +1,15 @@
+<html><body><?php
+    
+    $var = "World!";
+    
+    echo "Hello, $var<br>";
+    echo "Hello, " . $var . "<br>";
+    
+    //comment
+    #comment
+    /*comment*/
+    
+    
+    
+    
+</html></body><?
