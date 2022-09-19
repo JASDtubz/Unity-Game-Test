bddf92834706f7c6df7567af15ec347b3f6a5d26
JASD <86506578+JASDtubz@users.noreply.github.com>
2022-09-19 13:16:21 -0500

    Update test.php

--- idytl/test.php
+++ idytl/test.php
@@ -1,6 +1,8 @@
 <html><body><?php
     
     $var = "World!";
+    $int = 0;
+    $float = 0.0;
     
     echo "Hello, $var<br>";
     echo "Hello, " . $var . "<br>";
@@ -9,7 +11,17 @@
     #comment
     /*comment*/
     
+    $int = 9;
+    $float = 12.0;
     
+    echo $int + $float + "<br>";
     
+    printFunc();
+    
+    function printFunc()
+    {
+        global $var;
+        echo "Hello, $var<br>";
+    }
     
 </html></body><?
